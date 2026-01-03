9f8ff6c77e008c1973a98b1af100f251343eed21
Nikhil <nikhilkurrey135@gmail.com>
2026-01-03 17:35:51 +0530

    HeroSection started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    // very first step empty fragment
+    <>
+    <Hero />
+    </>
+  )
+}
+
+export default App
